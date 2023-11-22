cc6ae100b5492f0336249c07658bbd1cc2ef6cc7
LemonOD <t.lemon@semicolon.africa>
2023-11-22 21:27:16 +0100

    alt

--- alternate.py
+++
@@ -1,6 +0,0 @@
-def alternate_elements(input_list):
-    return input_list[::2]
-
-# Test cases
-print(alternate_elements([1, 2, 3, 4, 5, 6]))       
-print(alternate_elements(['a', 'b', 'c', 'd', 'e']))  
