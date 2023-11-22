725494096211320944130a24a29af2e2b19eff85
LemonOD <t.lemon@semicolon.africa>
2023-11-22 21:27:30 +0100

    j

---
+++ alternate.py
@@ -0,0 +1,6 @@
+def alternate_elements(input_list):
+    return input_list[::2]
+
+# Test cases
+print(alternate_elements([1, 2, 3, 4, 5, 6]))       
+print(alternate_elements(['a', 'b', 'c', 'd', 'e']))  
